da83f75d11fb13bc7d248e362f1a076a60fd7225
Matthew Tullo <mtullo1@binghamton.edu>
2025-04-11 18:48:09 -0400

    update draft client

---
+++ draft-league-client/src/index.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import store from './store.jsx'
+import { Provider } from 'react-redux'
+
+export default function Main(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <Main />
+  </Provider>,
+)
